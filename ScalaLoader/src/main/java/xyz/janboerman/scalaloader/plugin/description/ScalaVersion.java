b299a76f21bb4086c526f34adf1ae4215d3e1e26
EpiCanard <epicanard@gmail.com>
2019-09-01 17:55:50 +0200

    Get back unstable scala version and set them as deprecated

--- ScalaLoader/src/main/java/xyz/janboerman/scalaloader/plugin/description/ScalaVersion.java
+++ ScalaLoader/src/main/java/xyz/janboerman/scalaloader/plugin/description/ScalaVersion.java
@@ -19,6 +19,16 @@ public enum ScalaVersion {
             "https://bintray.com/bintray/jcenter/download_file?file_path=org%2Fscala-lang%2Fscala-reflect%2F2.12.8%2Fscala-reflect-2.12.8.jar",
             "https://bintray.com/bintray/jcenter/download_file?file_path=org%2Fscala-lang%2Fscala-library%2F2.12.8%2Fscala-library-2.12.8.jar"
     ),
+    @Deprecated
+    v2_13_0_M4("2.13.0-M4", false,
+            "https://bintray.com/bintray/jcenter/download_file?file_path=org%2Fscala-lang%2Fscala-reflect%2F2.13.0-M4%2Fscala-reflect-2.13.0-M4.jar",
+            "https://bintray.com/bintray/jcenter/download_file?file_path=org%2Fscala-lang%2Fscala-library%2F2.13.0-M4%2Fscala-library-2.13.0-M4.jar"
+    ),
+    @Deprecated
+    v2_13_0_M5("2.13.0-M5", false,
+            "https://bintray.com/bintray/jcenter/download_file?file_path=org%2Fscala-lang%2Fscala-reflect%2F2.13.0-M5%2Fscala-reflect-2.13.0-M5.jar",
+            "https://bintray.com/bintray/jcenter/download_file?file_path=org%2Fscala-lang%2Fscala-library%2F2.13.0-M5%2Fscala-library-2.13.0-M5.jar"
+    ),
     v2_13_0("2.13.0", true,
             "https://bintray.com/bintray/jcenter/download_file?file_path=org%2Fscala-lang%2Fscala-reflect%2F2.13.0%2Fscala-reflect-2.13.0.jar",
             "https://bintray.com/bintray/jcenter/download_file?file_path=org%2Fscala-lang%2Fscala-library%2F2.13.0%2Fscala-library-2.13.0.jar"
