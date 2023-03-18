1b88d7ebaa55e3f77953e197f5a69412c7c1d28e
Jan Boerman <janboerman95@gmail.com>
2023-03-18 13:59:36 +0100

    split the project up in bukkit-specific and paper-specific code

---
+++ ScalaLoader-Paper/src/main/java/xyz/janboerman/scalaloader/plugin/paper/PaperHacks.java
@@ -0,0 +1,15 @@
+package xyz.janboerman.scalaloader.plugin.paper;
+
+import io.papermc.paper.plugin.provider.configuration.PaperPluginMeta;
+import io.papermc.paper.plugin.provider.type.PluginFileType;
+import io.papermc.paper.plugin.provider.type.PluginTypeFactory;
+import io.papermc.paper.plugin.provider.type.paper.PaperPluginParent;
+
+import java.lang.reflect.Field;
+
+//TODO will I even need this?
+public class PaperHacks {
+
+    private PaperHacks() {}
+
+}
