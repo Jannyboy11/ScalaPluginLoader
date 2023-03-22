b96f283048efbc6673f681947040f4322fc078a6
Jan Boerman <janboerman95@gmail.com>
2023-03-22 21:11:25 +0100

    fix NPE

--- ScalaLoader-Common/src/main/java/xyz/janboerman/scalaloader/util/ListOps.java
+++ ScalaLoader-Common/src/main/java/xyz/janboerman/scalaloader/util/ListOps.java
@@ -8,7 +8,9 @@ public class ListOps {
     private ListOps() {
     }
 
-    public static <T> List<T> concat(List<T> one, List<T> two){
+    public static <T> List<T> concat(List<T> one, List<T> two) {
+        if (one == null) return two;
+        if (two == null) return one;
         ArrayList<T> arrayList = new ArrayList<T>(one.size() + two.size());
         arrayList.addAll(one);
         arrayList.addAll(two);
