1ef7a5fc19d39aaf755635b9dbb9ed45dd0133bd
Jan Boerman <jan.boerman@technolution.nl>
2024-02-13 00:25:50 +0100

    Make it compile for Bukkit/Paper 1.20.4.

--- ScalaLoader-Paper/src/main/java/xyz/janboerman/scalaloader/paper/plugin/ScalaPluginBootstrapContext.java
+++ ScalaLoader-Paper/src/main/java/xyz/janboerman/scalaloader/paper/plugin/ScalaPluginBootstrapContext.java
@@ -1,12 +1,37 @@
 package xyz.janboerman.scalaloader.paper.plugin;
 
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.configuration.PluginMeta;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.handler.configuration.LifecycleEventHandlerConfiguration;
 import xyz.janboerman.scalaloader.plugin.ScalaPluginDescription;
 
 import java.io.File;
 
+import org.jetbrains.annotations.NotNull;
+
 public class ScalaPluginBootstrapContext extends ScalaPluginProviderContext implements BootstrapContext {
+
+    private final LifecycleEventManager<BootstrapContext> lifecycleEventManager = new LifecycleEventManager<BootstrapContext>() {
+        @Override
+        public void registerEventHandler(
+                @NotNull LifecycleEventHandlerConfiguration<? super BootstrapContext> handlerConfiguration) {
+            //TODO this is currently not called. when should this be called? should it be called at all?
+        }
+    };
+
+
     public ScalaPluginBootstrapContext(File pluginJarFile, ScalaPluginDescription description) {
         super(pluginJarFile, description);
+    }
+
+    @Override
+    public @NotNull LifecycleEventManager<BootstrapContext> getLifecycleManager() {
+        return lifecycleEventManager;
+    }
+
+    @Override
+    public @NotNull PluginMeta getPluginMeta() {
+        return getConfiguration();
     }
 }
